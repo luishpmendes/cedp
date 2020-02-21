luishpmendes/mastersproject

ArtificialInstanceGenerator exec
Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

Makefile
@@ -21,6 +21,17 @@ $(BIN)/test/InstanceTest: $(BIN)/disjoint-sets/DisjointSets.o \
 	$(MKDIR) $(@D)
 	$(CPP) -o $@ $^ $(CARGS)
 
+$(BIN)/exec/ArtificialInstanceGeneratorExec: $(BIN)/disjoint-sets/DisjointSets.o \
+                                             $(BIN)/graph/Vertex.o \
+                                             $(BIN)/graph/Edge.o \
+                                             $(BIN)/graph/Graph.o \
+                                             $(BIN)/instance/Instance.o \
+                                             $(BIN)/exec/ArgumentParser.o \
+                                             $(BIN)/exec/ArtificialInstanceGeneratorExec.o
+	$(MKDIR) $(@D)
+	$(CPP) -o $@ $^ $(CARGS)
 
 InstanceTest: $(BIN)/test/InstanceTest
+
+ArtificialInstanceGeneratorExec: $(BIN)/exec/ArtificialInstanceGeneratorExec
 

luishpmendes/cedp

Makefile
@@ -31,7 +31,19 @@ $(BIN)/exec/GraphGeneratorExec: $(BIN)/disjoint-sets/DisjointSets.o \
 	$(MKDIR) $(@D)
 	$(CPP) -o $@ $^ $(CARGS)
 
+$(BIN)/exec/InstanceGeneratorExec: $(BIN)/disjoint-sets/DisjointSets.o \
+                                   $(BIN)/graph/Vertex.o \
+                                   $(BIN)/graph/Edge.o \
+                                   $(BIN)/graph/Graph.o \
+                                   $(BIN)/instance/Instance.o \
+                                   $(BIN)/exec/ArgumentParser.o \
+                                   $(BIN)/exec/InstanceGeneratorExec.o
+	$(MKDIR) $(@D)
+	$(CPP) -o $@ $^ $(CARGS)
+
 InstanceTest: $(BIN)/test/InstanceTest
 
 GraphGeneratorExec: $(BIN)/exec/GraphGeneratorExec
+
+InstanceGeneratorExec: $(BIN)/exec/InstanceGeneratorExec
 