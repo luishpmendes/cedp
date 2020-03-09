luishpmendes/mastersproject

LinearRelaxationSolver test
Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

Makefile
@@ -3,6 +3,8 @@ CARGS=-std=c++14 -Wall -Werror -O0 -g3 -m64
 DLIBINC=/opt/dlib-19.17/
 DLIB=-lpthread -lX11
 CXXFILES=/opt/dlib-19.17/dlib/all/source.cpp
+GRBINC=/opt/gurobi811/linux64/include/
+GRBLIB=-L/opt/gurobi811/linux64/lib -lgurobi_c++ -lgurobi81 -lm
 MKDIR=mkdir -p
 RM=rm -rf
 SRC=$(PWD)/src
@@ -13,7 +15,7 @@ clean:
 
 $(BIN)/%.o: $(SRC)/%.cpp
 	$(MKDIR) $(@D)
-	$(CPP) $(CARGS) -c $< -o $@ -I$(DLIBINC)
+	$(CPP) $(CARGS) -c $< -o $@ -I$(DLIBINC) -I$(GRBINC) $(GRBLIB)
 
 $(BIN)/test/InstanceTest: $(BIN)/disjoint-sets/DisjointSets.o \
                           $(BIN)/graph/Vertex.o \
@@ -58,6 +60,22 @@ $(BIN)/test/HeuristicTest: $(BIN)/disjoint-sets/DisjointSets.o \
 	$(MKDIR) $(@D)
 	$(CPP) $(CXXFILES) -o $@ $^ $(CARGS) $(DLIB)
 
+$(BIN)/test/LinearRelaxationSolverTest: $(BIN)/disjoint-sets/DisjointSets.o \
+                                        $(BIN)/graph/Vertex.o \
+                                        $(BIN)/graph/Edge.o \
+                                        $(BIN)/graph/Graph.o \
+                                        $(BIN)/instance/Instance.o \
+                                        $(BIN)/solution/Solution.o \
+                                        $(BIN)/solver/heuristic/Heuristic.o \
+                                        $(BIN)/solver/heuristic/constructive/GreedyConstructiveHeuristic.o \
+                                        $(BIN)/solver/heuristic/fixer/SolutionFixer.o \
+                                        $(BIN)/solver/heuristic/localsearch/LocalSearchHeuristic.o \
+                                        $(BIN)/solver/CEDPSolver.o \
+                                        $(BIN)/solver/metaheuristic/linear-relaxation/LinearRelaxationSolver.o \
+                                        $(BIN)/test/LinearRelaxationSolverTest.o
+	$(MKDIR) $(@D)
+	$(CPP) $(CXXFILES) -o $@ $^ $(CARGS) $(DLIB) -I$(GRBINC) $(GRBLIB)
+
 InstanceTest: $(BIN)/test/InstanceTest
 
 ArtificialInstanceGeneratorExec: $(BIN)/exec/ArtificialInstanceGeneratorExec
@@ -65,4 +83,6 @@ ArtificialInstanceGeneratorExec: $(BIN)/exec/ArtificialInstanceGeneratorExec
 SolutionTest: $(BIN)/test/SolutionTest
 
 HeuristicTest: $(BIN)/test/HeuristicTest
+
+LinearRelaxationSolverTest: $(BIN)/test/LinearRelaxationSolverTest
 

luishpmendes/cedp

Makefile
@@ -3,6 +3,8 @@ CARGS=-std=c++14 -Wall -Werror -O0 -g3 -m64
 DLIBINC=/opt/dlib-19.19/
 DLIB=-lpthread -lX11
 CXXFILES=/opt/dlib-19.19/dlib/all/source.cpp
+GRBINC=/opt/gurobi901/linux64/include/
+GRBLIB=-L/opt/gurobi901/linux64/lib -lgurobi_c++ -lgurobi90 -lm
 MKDIR=mkdir -p
 RM=rm -rf
 SRC=$(PWD)/src
@@ -13,7 +15,7 @@ clean:
 
 $(BIN)/%.o: $(SRC)/%.cpp
 	$(MKDIR) $(@D)
-	$(CPP) $(CARGS) -c $< -o $@ -I$(DLIBINC)
+	$(CPP) $(CARGS) -c $< -o $@ -I$(DLIBINC) -I$(GRBINC) $(GRBLIB)
 
 $(BIN)/test/InstanceTest: $(BIN)/disjoint-sets/DisjointSets.o \
                           $(BIN)/graph/Vertex.o \
@@ -68,6 +70,22 @@ $(BIN)/test/HeuristicTest: $(BIN)/disjoint-sets/DisjointSets.o \
 	$(MKDIR) $(@D)
 	$(CPP) $(CXXFILES) -o $@ $^ $(CARGS) $(DLIB)
 
+$(BIN)/test/LinearRelaxationSolverTest: $(BIN)/disjoint-sets/DisjointSets.o \
+                                        $(BIN)/graph/Vertex.o \
+                                        $(BIN)/graph/Edge.o \
+                                        $(BIN)/graph/Graph.o \
+                                        $(BIN)/instance/Instance.o \
+                                        $(BIN)/solution/Solution.o \
+                                        $(BIN)/solver/heuristic/Heuristic.o \
+                                        $(BIN)/solver/heuristic/constructive/GreedyConstructiveHeuristic.o \
+                                        $(BIN)/solver/heuristic/fixer/SolutionFixer.o \
+                                        $(BIN)/solver/heuristic/localsearch/LocalSearchHeuristic.o \
+                                        $(BIN)/solver/CEDPSolver.o \
+                                        $(BIN)/solver/metaheuristic/linear-relaxation/LinearRelaxationSolver.o \
+                                        $(BIN)/test/LinearRelaxationSolverTest.o
+	$(MKDIR) $(@D)
+	$(CPP) $(CXXFILES) -o $@ $^ $(CARGS) $(DLIB) -I$(GRBINC) $(GRBLIB)
+
 InstanceTest: $(BIN)/test/InstanceTest
 
 GraphGeneratorExec: $(BIN)/exec/GraphGeneratorExec
@@ -77,4 +95,6 @@ InstanceGeneratorExec: $(BIN)/exec/InstanceGeneratorExec
 SolutionTest: $(BIN)/test/SolutionTest
 
 HeuristicTest: $(BIN)/test/HeuristicTest
+
+LinearRelaxationSolverTest: $(BIN)/test/LinearRelaxationSolverTest
 