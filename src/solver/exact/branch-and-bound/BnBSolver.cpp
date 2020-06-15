6f1f4175ef05ed567dbb01e4419888512598ed0b
Luis H. P. Mendes <luishpmendes@gmail.com>
2020-06-14 23:45:22 -0300

    fixup! BnBSolver class

--- src/solver/exact/branch-and-bound/BnBSolver.cpp
+++ src/solver/exact/branch-and-bound/BnBSolver.cpp
@@ -23,33 +23,17 @@ BnBSolver::BnBSolver() : CEDPSolver::CEDPSolver() {}
 void BnBSolver::solve() {
     this->startTime = std::chrono::steady_clock::now();
 
-    this->bestPrimalSolution = this->gcHeuristic.constructSolution(round(
-                0.05 * ((double) this->timeLimit)));
-
-    if (!this->bestPrimalSolution.isFeasible()) {
-        this->bestPrimalSolution = SolutionFixer::fixSolution(
-                this->bestPrimalSolution, 
-                round(0.05 * ((double) this->timeLimit)));
-    }
-
-    if (this->bestPrimalSolution.isFeasible()) {
-        this->solutionsCounter++;
-        this->bestPrimalBound = this->bestPrimalSolution.getValue();
-    }
-
     GRBEnv * env = 0;
 
     try {
         env = new GRBEnv();
         GRBModel model = GRBModel(* env);
 
-        std::vector<std::vector<GRBVar> > x (this->instance.getLG().n(), 
-                std::vector<GRBVar> (this->instance.getM()));
-        std::vector<std::vector<GRBVar> > y (this->instance.getLG().n(), 
-                std::vector<GRBVar> (this->instance.getM()));
-        std::vector<std::vector<GRBVar> > z (
-                2 * this->instance.getGPrime().m(), 
-                std::vector<GRBVar> (this->instance.getM()));
+        std::vector<std::vector<GRBVar> > x (this->instance.getLG().n(), std::vector<GRBVar>
+                (this->instance.getM()));
+        std::vector<GRBVar> y (this->instance.getLG().n());
+        std::vector<std::vector<GRBVar> > z (2 * this->instance.getGPrime().m(), std::vector<GRBVar>
+                (this->instance.getM()));
 
         std::set<Vertex> vertices = this->instance.getLG().getVertices();
 
@@ -62,21 +46,18 @@ void BnBSolver::solve() {
                 unsigned int obj = this->instance.getC(eId, j);
 
                 /* x_{e, j} ∈ {0, 1} */
-                x[eId - 1][j] = model.addVar(0.0, 1.0, obj, GRB_BINARY, "x_" + 
-                        std::to_string(eId - 1) + "_" + std::to_string(j));
+                x[eId - 1][j] = model.addVar(0.0, 1.0, obj, GRB_BINARY, "x_" + std::to_string(eId -
+                            1) + "_" + std::to_string(j));
             }
         }
 
-        /* ∀ {e′, e} ∈ A′\A */
+        /* ∀ e ∈ U */
         for (const Vertex & e : vertices) {
             unsigned int eId = this->instance.getLG().getVertexId(e);
 
-            /* ∀ j ∈ {1, ..., m} */
-            for (unsigned int j = 0; j < this->instance.getM(); j++) {
-                /* y_{{e′, e}, j} ∈ {0, 1} */
-                y[eId - 1][j] = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "x_" + 
-                        std::to_string(eId - 1) + "_" + std::to_string(j));
-            }
+                /* y_{e} ∈ {0, 1} */
+                y[eId - 1] = model.addVar(0.0, 1.0, 0.0, GRB_BINARY, "x_" + std::to_string(eId -
+                            1));
         }
 
         /* ∀ {e, f} ∈ A′ */
@@ -85,16 +66,15 @@ void BnBSolver::solve() {
 
             /* ∀ j ∈ {1, ..., m} */
             for (unsigned int j = 0; j < this->instance.getM(); j++) {
-                /* y_{e, f, j} ∈ [0, |U|] */
-                z[efId - 1][j] = model.addVar(0.0, this->instance.getLG().n(), 
-                        0.0, GRB_CONTINUOUS, "y_" + std::to_string(efId - 1) + 
-                        "_" + std::to_string(j));
+                /* z_{e, f, j} ∈ [0, |U|] */
+                z[efId - 1][j] = model.addVar(0.0, this->instance.getLG().n(), 0.0, GRB_CONTINUOUS,
+                        "z_" + std::to_string(efId - 1) + "_" + std::to_string(j));
 
-                /* y_{f, e, j} ∈ [0, |U|] */
-                z[this->instance.getGPrime().m() + efId - 1][j] = model.addVar(
-                        0.0, this->instance.getLG().n(), 0.0, GRB_CONTINUOUS, 
-                        "y_" + std::to_string(this->instance.getGPrime().m() + 
-                            efId - 1) + "_" + std::to_string(j));
+                /* z_{f, e, j} ∈ [0, |U|] */
+                z[this->instance.getGPrime().m() + efId - 1][j] = model.addVar(0.0,
+                        this->instance.getLG().n(), 0.0, GRB_CONTINUOUS, "z_" +
+                        std::to_string(this->instance.getGPrime().m() + efId - 1) + "_" +
+                        std::to_string(j));
             }
         }
 
@@ -108,13 +88,13 @@ void BnBSolver::solve() {
             unsigned int eId = this->instance.getLG().getVertexId(e);
 
             /* ∑_{j = 1}^{m}{x_{e, j}} = 1 */
-            GRBLinExpr constr01 = 0.0;
+            GRBLinExpr lhs1 = 0.0;
 
             for (unsigned int j = 0; j < this->instance.getM(); j++) {
-                constr01 += x[eId - 1][j];
+                lhs1 += x[eId - 1][j];
             }
 
-            model.addConstr(constr01 == 1, "c_01_" + std::to_string(eId - 1));
+            model.addConstr(lhs1 == 1, "c_1_" + std::to_string(eId - 1));
         }
 
         /* ∀ j ∈ {1, ..., m} */
@@ -123,17 +103,16 @@ void BnBSolver::solve() {
              * 2 * ∑_{e ∈ U}{d_{e} * x_{e, j}} <= 
              * min{D, ((2 * (1 + B)) / m) * ∑_{e ∈ U}{d_{e}}
              */
-            GRBLinExpr constr02 = 0.0;
+            GRBLinExpr lhs2 = 0.0;
 
             for (const Vertex & e : vertices) {
                 unsigned int eId = this->instance.getLG().getVertexId(e);
 
-                constr02 += 2.0 * e.w * x[eId - 1][j];
+                lhs2 += 2.0 * e.w * x[eId - 1][j];
             }
 
-            double rhs = this->instance.getMaximumDemand();
-
-            model.addConstr(constr02 <= rhs, "c_02_" + std::to_string(j));
+            model.addConstr(lhs2 <= this->instance.getMaximumDemand(), "c_2_" + 
+                    std::to_string(j));
         }
 
         /* ∀ j ∈ {1, ..., m} */
@@ -142,78 +121,36 @@ void BnBSolver::solve() {
              * 2 * ∑_{e ∈ U}{d_{e} * x_{e, j}} >= 
              * ((2 * (1 - B)) / m) * ∑_{e ∈ U}{d_{e}}
              */
-            GRBLinExpr constr03 = 0.0;
+            GRBLinExpr lhs3 = 0.0;
 
             for (const Vertex & e : vertices) {
                 unsigned int eId = this->instance.getLG().getVertexId(e);
 
-                constr03 += 2.0 * e.w * x[eId - 1][j];
+                lhs3 += 2.0 * e.w * x[eId - 1][j];
             }
 
-            double rhs = this->instance.getMinimumDemand();
-
-            model.addConstr(constr03 >= rhs, "c_03_" + std::to_string(j));
+            model.addConstr(lhs3 >= this->instance.getMinimumDemand(), "c_3_" + 
+                    std::to_string(j));
         }
 
-        /* ∀ j ∈ {1, ..., m} */
-        for (unsigned int j = 0; j < this->instance.getM(); j++) {
-            /* ∑_{e ∈  U}{y_{{e′, e}, j}} = 1 */
-            GRBLinExpr constr04 = 0.0;
+        /* ∑_{e ∈  U}{y_{e}} = m */
+        GRBLinExpr lhs4 = 0.0;
 
-            for (const Vertex & e : vertices) {
-                unsigned int eId = this->instance.getLG().getVertexId(e);
+        for (const Vertex & e : vertices) {
+            unsigned int eId = this->instance.getLG().getVertexId(e);
 
-                constr04 += y[eId - 1][j];
-            }
-
-            model.addConstr(constr04 == 1, "c_04_" + std::to_string(j));
+            lhs4 += y[eId - 1];
         }
 
-
-        /* ∀ j ∈ {1, ..., m} */
-        for (unsigned int j = 0; j < this->instance.getM(); j++) {
-            /* ∀ e ∈ U */
-            for (const Vertex & e : vertices) {
-                unsigned int eId = this->instance.getLG().getVertexId(e);
-
-                /* x_{e, j} - y_{{e′, e}, j} >= 0 */
-                GRBLinExpr constr05 = x[eId - 1][j] - y[eId - 1][j];
-                model.addConstr(constr05 >= 0.0, "c_05_" + 
-                        std::to_string(eId) + "_" + std::to_string(j));
-            }
-        }
-
-        /* ∀ j ∈ {1, ..., m} */
-        for (unsigned int j = 0; j < this->instance.getM(); j++) {
-            /* ∀ e ∈ U */
-            for (std::set<Vertex>::iterator eIt = vertices.begin(); 
-                    eIt != vertices.end(); eIt++) {
-                unsigned int eId = this->instance.getLG().getVertexId(*eIt);
-
-                /* ∀ f ∈ U, f > e */
-                for (std::set<Vertex>::iterator fIt = std::next(eIt); 
-                        fIt != vertices.end(); fIt++) {
-                    unsigned int fId = 
-                        this->instance.getLG().getVertexId(*fIt);
-
-                    GRBLinExpr constr06 = x[eId - 1][j] - y[eId - 1][j] + 
-                        y[fId - 1][j];
-                    model.addConstr(constr06 <= 1.0, "c_06_" + 
-                            std::to_string(fId) + "_" + std::to_string(eId) + 
-                            "_" + std::to_string(j));
-                }
-            }
-        }
+        model.addConstr(lhs4 == this->instance.getM(), "c_4");
 
         /* ∑_{j = 1}^{m}{∑_{e ∈ U}{z_{e′, e, j}}} = |U| */
-        GRBLinExpr constr07 = 0.0;
+        GRBLinExpr lhs5 = 0.0;
 
         for (unsigned int j = 0; j < this->instance.getM(); j++) {
-            for (const Edge & ePrimeE : 
-                    this->instance.getGPrime().getIncidentEdges(
-                        this->instance.getEPrime())) {
-                unsigned int ePrimeEId = 
-                    this->instance.getGPrime().getEdgeId(ePrimeE);
+            for (const Edge & ePrimeE :
+                    this->instance.getGPrime().getIncidentEdges(this->instance.getEPrime())) {
+                unsigned int ePrimeEId = this->instance.getGPrime().getEdgeId(ePrimeE);
                 Vertex e = ePrimeE.u;
 
                 if (e == this->instance.getEPrime()) {
@@ -221,23 +158,21 @@ void BnBSolver::solve() {
                 }
 
                 if (this->instance.getEPrime() < e) {
-                    constr07 += z[ePrimeEId - 1][j];
+                    lhs5 += z[ePrimeEId - 1][j];
                 } else {
-                    constr07 += z[this->instance.getGPrime().m() + ePrimeEId - 1][j];
+                    lhs5 += z[this->instance.getGPrime().m() + ePrimeEId - 1][j];
                 }
             }
         }
 
-        model.addConstr(constr07 == this->instance.getLG().n(), "c_07");
+        model.addConstr(lhs5 == this->instance.getLG().n(), "c_5");
 
         unsigned int M = 0;
         double sumD = 0;
         std::vector<Vertex> verticesVector (vertices.begin(), vertices.end());
-        std::sort(verticesVector.begin(), verticesVector.end(), 
-                Vertex::weightComparator);
+        std::sort(verticesVector.begin(), verticesVector.end(), Vertex::weightComparator);
 
-        for (M = 0; M < verticesVector.size() && 
-                sumD < this->instance.getMaximumDemand(); M++) {
+        for (M = 0; M < verticesVector.size() && sumD < this->instance.getMaximumDemand(); M++) {
             sumD += verticesVector[M].w;
         }
 
@@ -254,22 +189,22 @@ void BnBSolver::solve() {
         for (unsigned int j = 0; j < this->instance.getM(); j++) {
             for (const Vertex & e : vertices) {
                 unsigned int eId = this->instance.getLG().getVertexId(e);
-                unsigned int ePrimeEId = this->instance.getGPrime().getEdgeId(
-                        this->instance.getEPrime(), e);
+                unsigned int ePrimeEId =
+                    this->instance.getGPrime().getEdgeId(this->instance.getEPrime(), e);
 
-                /* z_{e′, e, j} - |U| y_{{e′, e}, j} <= 0 */
-                GRBLinExpr constr08 = 0.0;
+                /* z_{e′, e, j} - |U| y_{e} <= 0 */
+                GRBLinExpr lhs6 = 0.0;
 
                 if (e > this->instance.getEPrime()) {
-                    constr08 += z[ePrimeEId - 1][j];
+                    lhs6 += z[ePrimeEId - 1][j];
                 } else {
-                    constr08 += z[this->instance.getGPrime().m() + ePrimeEId - 1][j];
+                    lhs6 += z[this->instance.getGPrime().m() + ePrimeEId - 1][j];
                 }
 
-                constr08 -= M * y[eId - 1][j];
+                lhs6 -= M * y[eId - 1];
 
-                model.addConstr(constr08 <= 0, "c_08_" + std::to_string(eId) 
-                        + "_" + std::to_string(j));
+                model.addConstr(lhs6 <= 0, "c_6_" + std::to_string(eId) + "_" + 
+                        std::to_string(j));
             }
         }
 
@@ -291,19 +226,18 @@ void BnBSolver::solve() {
                     unsigned int fId = this->instance.getLG().getVertexId(f);
 
                     /* z_{e, f, j} - |U| * x_{e, j} <= 0 */
-                    GRBLinExpr constr09 = 0.0;
+                    GRBLinExpr lhs7 = 0.0;
 
                     if (e < f) {
-                        constr09 += z[efId - 1][j];
+                        lhs7 += z[efId - 1][j];
                     } else {
-                        constr09 += z[this->instance.getGPrime().m() + efId - 1][j];
+                        lhs7 += z[this->instance.getGPrime().m() + efId - 1][j];
                     }
 
-                    constr09 -= M * x[eId - 1][j];
+                    lhs7 -= M * x[eId - 1][j];
 
-                    model.addConstr(constr09 <= 0, "c_09_" + 
-                            std::to_string(fId) + "_" + std::to_string(eId) + 
-                            "_" + std::to_string(j));
+                    model.addConstr(lhs7 <= 0, "c_7_" + std::to_string(fId) + "_" +
+                            std::to_string(eId) + "_" + std::to_string(j));
                 }
             }
         }
@@ -326,19 +260,18 @@ void BnBSolver::solve() {
                     unsigned int fId = this->instance.getLG().getVertexId(f);
 
                     /* z_{f, e, j} - |U| * x_{e, j} <= 0 */
-                    GRBLinExpr constr10 = 0.0;
+                    GRBLinExpr lhs8 = 0.0;
 
                     if (f < e) {
-                        constr10 += z[efId - 1][j];
+                        lhs8 += z[efId - 1][j];
                     } else {
-                        constr10 += z[this->instance.getGPrime().m() + efId - 1][j];
+                        lhs8 += z[this->instance.getGPrime().m() + efId - 1][j];
                     }
 
-                    constr10 -= M * x[eId - 1][j];
+                    lhs8 -= M * x[eId - 1][j];
 
-                    model.addConstr(constr10 <= 0, "c_10_" + 
-                            std::to_string(eId) + "_" + std::to_string(fId) + 
-                            "_" + std::to_string(j));
+                    model.addConstr(lhs8 <= 0, "c_8_" + std::to_string(eId) + "_" +
+                            std::to_string(fId) + "_" + std::to_string(j));
                 }
             }
         }
@@ -350,9 +283,9 @@ void BnBSolver::solve() {
                 unsigned int eId = this->instance.getLG().getVertexId(e);
                 /*
                  * ∑_{f ∈ σ_{G′}(e)}{z_{e, f, j}} - 
-                 * ∑_{f ∈ σ_{G′}(e)}{y_{f, e, j}} + x_{e, j} = 0 
+                 * ∑_{f ∈ σ_{G′}(e)}{z_{f, e, j}} + x_{e, j} = 0 
                  */
-                GRBLinExpr constr11 = 0.0;
+                GRBLinExpr lhs9 = 0.0;
 
                 for (const Edge & ef : this->instance.getGPrime().getIncidentEdges(e)) {
                     unsigned int efId = this->instance.getGPrime().getEdgeId(ef);
@@ -363,9 +296,9 @@ void BnBSolver::solve() {
                     }
 
                     if (f < e) {
-                        constr11 -= z[efId - 1][j];
+                        lhs9 -= z[efId - 1][j];
                     } else {
-                        constr11 -= z[this->instance.getGPrime().m() + efId - 1][j];
+                        lhs9 -= z[this->instance.getGPrime().m() + efId - 1][j];
                     }
                 }
 
@@ -378,33 +311,16 @@ void BnBSolver::solve() {
                     }
 
                     if (e < f) {
-                        constr11 += z[efId - 1][j];
+                        lhs9 += z[efId - 1][j];
                     } else {
-                        constr11 += z[this->instance.getGPrime().m() + efId - 1][j];
+                        lhs9 += z[this->instance.getGPrime().m() + efId - 1][j];
                     }
                 }
 
-                constr11 += x[eId - 1][j];
+                lhs9 += x[eId - 1][j];
 
-                model.addConstr(constr11 == 0, "c_11_" +  std::to_string(eId) 
-                        + "_" + std::to_string(j));
-            }
-        }
-
-        for (const Vertex & e : vertices) {
-            unsigned int eId = this->instance.getLG().getVertexId(e);
-
-            for (unsigned int j = 0; j < this->instance.getM(); j++) {
-                x[eId - 1][j].set(GRB_DoubleAttr_Start, 0.0);
-            }
-        }
-
-
-        for (unsigned int j = 0; j < this->instance.getM(); j++) {
-            for (const Edge & e : this->bestPrimalSolution.getDistrict(j)) {
-                unsigned int eId = this->instance.getG().getEdgeId(e);
-
-                x[eId - 1][j].set(GRB_DoubleAttr_Start, 1.0);
+                model.addConstr(lhs9 == 0, "c_9_" +  std::to_string(eId) + "_" + 
+                        std::to_string(j));
             }
         }
 
@@ -417,8 +333,6 @@ void BnBSolver::solve() {
 
         model.set(GRB_DoubleParam_TimeLimit, ((double) remainingTime));
         model.set(GRB_IntParam_OutputFlag, 0);
-        model.set(GRB_IntParam_PoolSearchMode, 2);
-        model.set(GRB_IntParam_PoolSolutions, 16);
         model.set(GRB_IntParam_Threads, 1);
 
         model.update();
@@ -449,50 +363,6 @@ void BnBSolver::solve() {
             if (this->bestPrimalBound < primalBound) {
                 this->bestPrimalBound = primalBound;
                 this->bestPrimalSolution = solution;
-            }
-        } else {
-            this->bestDualBound = model.get(GRB_DoubleAttr_ObjBound);
-
-            unsigned int nSolutions = model.get(GRB_IntAttr_SolCount);
-
-            for (unsigned int i = 0; i < nSolutions; i++) {
-                std::vector<std::set<Edge> > districts (this->instance.getM());
-
-                model.set(GRB_IntParam_SolutionNumber, i);
-
-                for (unsigned int j = 0; j < this->instance.getM(); j++) {
-                    for (const Edge & e : this->instance.getG().getEdges()) {
-                        unsigned int eId = this->instance.getG().getEdgeId(e);
-
-                        if (x[eId - 1][j].get(GRB_DoubleAttr_Xn) >= 0.5) {
-                            districts[j].insert(e);
-                        }
-                    }
-                }
-
-                Solution solution = Solution(this->instance, districts);
-
-                this->solutionsCounter++;
-
-                elapsedTime = this->getElapsedTime();
-                remainingTime = 0;
-
-                if (this->timeLimit > elapsedTime) {
-                    remainingTime = this->timeLimit - elapsedTime;
-                }
-
-                solution = this->lsHeuristic.improveSolution(solution, remainingTime);
-
-                double primalBound = solution.getValue();
-
-                if (this->bestPrimalBound < primalBound) {
-                    this->bestPrimalBound = primalBound;
-                    this->bestPrimalSolution = solution;
-                }
-
-                if (this->areTerminationCriteriaMet()) {
-                    break;
-                }
             }
         }
     } catch (GRBException e) {
