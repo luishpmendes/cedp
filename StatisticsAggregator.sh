6b1d48ea46aa541cbc4604d504d5104526d4221e
Luis H. P. Mendes <luishpmendes@gmail.com>
2020-11-05 17:46:22 -0300

    Incrementing statistics aggregator script to use new solvers
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

--- StatisticsAggregator.sh
+++ StatisticsAggregator.sh
@@ -5,21 +5,29 @@ make StatisticsAggregatorExec;
 IFS=",";
 N=30;
 
-for solver in "BnBSolver" "BnCSolver" "GRASPSolver"
+for solver in "BnBSolverA" "BnCSolverA" "BnBSolverB" "BnCSolverB" "GRASPSolver"
 do
+    echo "solver="$solver;
     for type in "grid" "random"
     do
+        echo "type="$type;
         for m in 5 10
         do
+            echo "m="$m;
             for VE in 25,24 25,32 25,40 49,48 49,66 49,84 100,99 100,140 100,180
             do
                 set $VE;
                 V=$1;
+                echo "V="$V;
                 E=$2;
+                echo "E="$E;
                 for B in 0.5 1.0
                 do
+                    echo "B="$B;
                     for D in 0.5 1.0
                     do
+                        echo "D="$D;
+
                         ./bin/exec/StatisticsAggregatorExec --solver $solver \
                             --type $type -D $D \
                             < "statistics/"$type"-m"$m"V"$V"E"$E"B"${B//.}"D"${D//.}$solver".stat" \
