7167fcebb2c0a62d07281cfee45a6eecb0e6c122
Luis H. P. Mendes <luishpmendes@gmail.com>
2021-09-13 20:35:09 -0300

    Updating statistics aggregator
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

--- src/exec/StatisticsAggregatorExec.cpp
+++ src/exec/StatisticsAggregatorExec.cpp
@@ -2,11 +2,13 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main (int argc, char * argv[]) {
     ArgumentParser argParser (argc, argv);
 
-    if (argParser.cmdOptionExists("--type") && argParser.cmdOptionExists("--solver")) {
+    if (argParser.cmdOptionExists("--type") && 
+            argParser.cmdOptionExists("--solver")) {
         std::string type, solver;
         unsigned int m, D, V, E, U, A, UPrime, APrime, timeLimit, seed, 
                      solvingTime, solutionsFound, bestPrimalBound, 
@@ -14,8 +16,10 @@ int main (int argc, char * argv[]) {
                      firstSolutionIteration, firstSolutionTime, 
                      bestSolutionIteration, bestSolutionTime, notPartition, 
                      notConnected, dontRespectCapacity, notBalanced, 
-                     notFeasible, fixedSolutions, localSearchCounter;
-        double B, bestDualBound, maximumDemand, Dratio;
+                     notFeasible, fixedSolutions, localSearchCounter, psiSize,
+                     k, statisticalFilter, i;
+        double B, bestDualBound, maximumDemand, ratioMean, ratioStd, Dratio;
+        std::vector<double> psi, p;
 
         type = argParser.getCmdOption("--type");
         solver = argParser.getCmdOption("--solver");
@@ -39,9 +43,7 @@ int main (int argc, char * argv[]) {
         scanf("Best dual bound: %lf\n", &bestDualBound);
         scanf("Is solution feasible: %u\n", &isSolutionFeasible);
 
-        if (solver.compare("GRASPSolver") == 0 || 
-                solver.compare("LagrangianHeuristicSolver1") == 0 || 
-                solver.compare("LagrangianHeuristicSolver2") == 0) {
+        if (solver.compare("GRASPA") == 0 || solver.compare("GRASPB") == 0) {
             scanf("Total iterations: %u\n", &totalIterations);
             scanf("First solution iteration: %u\n", &firstSolutionIteration);
             scanf("First solution time: %u\n", &firstSolutionTime);
@@ -53,9 +55,24 @@ int main (int argc, char * argv[]) {
             scanf("Solutions that are not balanced: %u\n", &notBalanced);
             scanf("Solutions that are not feasible: %u\n", &notFeasible);
             scanf("Solutions that were fixed: %u\n", &fixedSolutions);
-            if (solver.compare("GRASPSolver") == 0) {
-                scanf("Local search counter: %u\n", &localSearchCounter);
+            scanf("Local search counter: %u\n", &localSearchCounter);
+            scanf("Ratio mean: %lf\n", &ratioMean);
+            scanf("Ratio standard deviation: %lf\n", &ratioStd);
+            scanf("m: %u\n", &psiSize);
+            scanf("k: %u\n", &k);
+            scanf("Statistical filter: %u\n", &statisticalFilter);
+            psi.resize(psiSize);
+            scanf("Psi: %lf", &psi[0]);
+            for (i = 1; i < psiSize - 1; i++) {
+                scanf(" %lf", &psi[i]);
             }
+            scanf(" %lf\n", &psi[psiSize - 1]);
+            p.resize(psiSize);
+            scanf("p: %lf", &p[0]);
+            for (i = 1; i < psiSize - 1; i++) {
+                scanf(" %lf", &p[i]);
+            }
+            scanf(" %lf\n", &p[psiSize - 1]);
         }
 
         std::cout << type << " " << Dratio << " " << solver << " " << m << " " 
@@ -65,21 +82,27 @@ int main (int argc, char * argv[]) {
             solutionsFound << " " << bestPrimalBound << " " << bestDualBound 
             << " " << isSolutionFeasible;
 
-        if (solver.compare("GRASPSolver") == 0 || 
-                solver.compare("LagrangianHeuristicSolver1") == 0 || 
-                solver.compare("LagrangianHeuristicSolver2") == 0) {
+        if (solver.compare("GRASPA") == 0 || solver.compare("GRASPB") == 0 ) {
             std::cout << " " << totalIterations << " " << 
                 firstSolutionIteration << " " << firstSolutionTime << " " << 
                 bestSolutionIteration << " " << bestSolutionTime << " " <<
                 notPartition << " " << notConnected << " " << 
                 dontRespectCapacity << " " << notBalanced << " " << 
-                notFeasible << " " << fixedSolutions;
-            if (solver.compare("GRASPSolver") == 0) {
-                std::cout << " " << localSearchCounter;
+                notFeasible << " " << fixedSolutions << " " << 
+                localSearchCounter << " " << ratioMean << " " << 
+                ratioStd << " " << psiSize << " " << k << " " << 
+                statisticalFilter;
+
+            for (i = 0; i < psi.size(); i++) {
+                std::cout << " " << psi[i];
+            }
+
+            for (i = 0; i < p.size(); i++) {
+                std::cout << " " << p[i];
             }
         }
-
-        std::cout << std::endl;
     }
+
+    std::cout << std::endl;
 }
 
